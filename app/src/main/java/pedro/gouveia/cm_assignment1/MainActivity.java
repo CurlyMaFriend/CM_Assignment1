ac9fb013532e1418061b802255b492af2cf86a63
pedrogouveia <pedrogouveia@student.dei.uc.pt>
2021-10-05 19:37:12 +0100

    Initial commit

---
+++ app/src/main/java/pedro/gouveia/cm_assignment1/MainActivity.java
@@ -0,0 +1,14 @@
+package pedro.gouveia.cm_assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
