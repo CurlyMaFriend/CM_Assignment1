0e5d7ba672843a65905226816dfb3e74c7ccbec9
jpcruz <jpcruz@student.dei.uc.pt>
2021-10-08 17:39:39 +0100

    Tentativa de envio de dados da Activity1 para Activity2

---
+++ app/src/main/java/pedro/gouveia/cm_assignment1/Activity2.java
@@ -0,0 +1,32 @@
+package pedro.gouveia.cm_assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+
+        Intent intent = getIntent();
+        String textOwner = intent.getStringExtra(MainActivity.EXTRA_TEXT1);
+        String textNome = intent.getStringExtra(MainActivity.EXTRA_TEXT2);
+        int numberIdade = intent.getIntExtra(MainActivity.EXTRA_NUMBER, 0);
+
+        TextView activity2Dono, activity2Nome, activity2Idade;
+
+        activity2Dono = (TextView) findViewById(R.id.activity2Dono);
+        activity2Nome = (TextView) findViewById(R.id.activity2Nome);
+        activity2Idade = (TextView) findViewById(R.id.activity2Idade);
+
+        activity2Dono.setText(textOwner);
+        activity2Nome.setText(textNome);
+        activity2Idade.setText(""+numberIdade);
+
+    }
+}
--- app/src/main/java/pedro/gouveia/cm_assignment1/MainActivity.java
+++ app/src/main/java/pedro/gouveia/cm_assignment1/MainActivity.java
@@ -2,13 +2,46 @@ package pedro.gouveia.cm_assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TEXT1 = "com.example.application.example.EXTRA_TEXT";
+    public static final String EXTRA_TEXT2 = "com.example.application.example.EXTRA_TEXT";
+    public static final String EXTRA_NUMBER = "com.example.application.example.EXTRA_NUMBER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToActivity(View view) {
+
+        TextView txtName, txtOwner, txtAge;
+        String conteudoNome, conteudoOwner;
+        int conteudoAge;
+
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtOwner = (TextView) findViewById(R.id.txtOwner);
+        txtAge = (TextView) findViewById(R.id.txtAge);
+
+        conteudoNome = txtName.getText().toString();
+        conteudoOwner = txtOwner.getText().toString();
+        conteudoAge = Integer.parseInt(txtAge.getText().toString());
+
+        Intent intent = new Intent(this, Activity2.class);
+
+        intent.putExtra(EXTRA_TEXT1, conteudoOwner);
+        intent.putExtra(EXTRA_TEXT2, conteudoNome);
+        intent.putExtra(EXTRA_NUMBER, conteudoAge);
+
+        startActivity(intent);
     }
 }
